c2253940358b16ea1445589fcf9e46274212ef87
AlexanderVetmanager <sacha.naza@yandex.ru>
2024-09-02 10:38:18 +0300

    init

---
+++ src/ExchangedAmount.php
@@ -0,0 +1,56 @@
+<?php
+
+use GuzzleHttp\Client;
+
+class ExchangedAmount
+{
+    private $from;
+    private $to;
+    private $amount;
+    private $client;
+
+    public function __construct(string $from, string $to, float $amount)
+    {
+        $this->from = $from;
+        $this->to = $to;
+        $this->amount = $amount;
+        $this->client = new Client([
+            'base_uri' => 'https://www.cbr.ru'
+        ]);
+    }
+
+    public function toDecimal()
+    {
+        $rates = $this->getRates();
+        $rate = $rates[$this->from][$this->to]['buy'];
+
+        return $this->amount * $rate;
+    }
+
+    private function getRates()
+    {
+        $response = $this->client->request('GET', '/scripts/XML_daily.asp');
+
+        if ($response->getStatusCode() !== 200) {
+            throw new \Exception('Error fetching exchange rates');
+        }
+
+        $xml = simplexml_load_string($response->getBody()->getContents());
+
+        $rates = [];
+
+        foreach ($xml->Valute as $valute) {
+            $charCode = (string) $valute->CharCode;
+            $rate = (float) str_replace(',', '.', $valute->Value);
+            $nominal = (int) $valute->Nominal;
+
+            if ($charCode === 'USD' || $charCode === 'EUR' || $charCode === 'RUB') {
+                $rates[$charCode]['UAH'] = [
+                    'buy' => $rate / $nominal
+                ];
+            }
+        }
+
+        return $rates;
+    }
+}
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitd751713988987e9331980363e24189ce::getLoader();
+return ComposerAutoloaderInitd6df618a6cfd14afccf9ea7e8b9ebc46::getLoader();
